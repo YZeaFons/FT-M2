a5bd75ad7e567e7ea466c8192b6b5aa27a50825a
YZeaFons <yeisonyamidzea@gmail.com>
2023-11-15 21:35:26 -0500

    first commit

---
+++ 06 - 07-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Species/Species.jsx
@@ -0,0 +1,18 @@
+import { log } from "@11ty/eleventy/src/EleventyErrorHandler";
+import React from "react";
+// import styledSpecies from "./Species.module.css";
+
+export default function Species({ species, handleSpecies, handleAllSpecies }) {
+  // console.log(species);
+  return (
+    <div>
+      <h2>Species</h2>
+      {
+        species.map((specie, index) => (
+          <button key={index} onClick={handleSpecies} value={specie}>{specie}</button>
+        ))
+      }
+      <button onClick={handleAllSpecies}>All Animals</button>
+    </div>
+  )
+}
