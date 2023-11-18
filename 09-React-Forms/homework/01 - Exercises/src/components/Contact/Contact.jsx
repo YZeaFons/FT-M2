c187dae6cacaca42ead547188ec0e1262ec707dc
YZeaFons <yeisonyamidzea@gmail.com>
2023-11-17 23:17:49 -0500

    homework charge

--- 09-React-Forms/homework/01 - Exercises/src/components/Contact/Contact.jsx
+++ 09-React-Forms/homework/01 - Exercises/src/components/Contact/Contact.jsx
@@ -1,9 +1,101 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Contact.modules.css'
+import validate from '../validation/validate'
 
 // eslint-disable-next-line
 const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
 
-export default function Contact () {
-  return <div>Crear Formulario</div>
+export default function Contact() {
+
+  const [inputs, setInputs] = React.useState({
+    name: '',
+    email: '',
+    message: ''
+  })
+  const [errors, setErrors] = React.useState({
+    name: '',
+    email: '',
+    message: ''
+  })
+
+  const handleChange = (event) => {
+    setInputs({
+      ...inputs,
+      // console.log(event);
+      [event.target.name]: event.target.value
+    })
+    setErrors(validate({
+      ...inputs,
+      [event.target.name]: event.target.value
+    }))
+  }
+  const handleSubmit = (event, valor = 1) => {
+    event.preventDefault();
+    valor = Object.keys(errors).length
+    if (valor !== 0) window.alert('Debe llenar todos los campos')
+    else {
+      window.alert('Datos completos')
+      setErrors({
+        name: '',
+        email: '',
+        message: ''
+      })
+      setInputs({
+        name: '',
+        email: '',
+        message: ''
+      })
+    }
+  }
+
+  console.log(errors);
+
+  return (
+    <div>
+
+      <h1>Informacion de contacto</h1>
+
+      <form onSubmit={handleSubmit}>
+        <label>Nombre:</label>
+        <input
+          type="text"
+          name='name'
+          placeholder='Escribe tu nombre...'
+          value={inputs.name}
+          onChange={handleChange}
+          className={errors.name && 'warning'}
+        />
+        <p className='danger'>{errors.name}</p>
+        <br />
+
+        <label>Correo Electrónico:</label>
+        <input
+          type="text"
+          name='email'
+          placeholder='Escribe tu email...'
+          value={inputs.email}
+          onChange={handleChange}
+          className={errors.email && 'warning'}
+        />
+        <p className='danger'>{errors.email}</p>
+        <br />
+
+        <label>Mensaje:</label>
+        <textarea
+          type="text"
+          name="message"
+          placeholder='Escribe tu mensaje...'
+          value={inputs.message}
+          onChange={handleChange}
+          className={errors.message && 'warning'}
+        >
+        </textarea>
+        <p className='danger'>{errors.message}</p>
+        <br />
+
+        <button type='submit'>Enviar</button>
+      </form>
+
+    </div>
+  )
 }
---
+++ 09-React-Forms/homework/01 - Exercises/src/components/validation/validate.js
@@ -0,0 +1,21 @@
+// import regexEmail from '../Contact/Contact'
+export default function validate(input) {
+
+    const errors = {}
+    const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+
+
+    if (input.name === '') errors.name = "Se requiere un nombre";
+
+    if (!regexEmail.test(input.email)) errors.email = "Debe ser un correo electrónico"
+
+    if (input.message === '') errors.message = 'Se requiere un mensaje';
+
+    return errors;
+}
+const pruebas = validate({
+    name: "",
+    email: "henry@gmail.com",
+    message: "Mensaje Henry",
+})
+console.log(pruebas);
